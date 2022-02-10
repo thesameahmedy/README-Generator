9fd7f902b835b02e57de841b35f9ce082b4a02c0
ahmed <mueed@Usc.edu>
2022-02-10 18:42:27 -0500

    Moved the ReadMe Generator Code to another file and will complete the project on the next push

--- index.js
+++ index.js
@@ -7,79 +7,82 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
-
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'What is the title of your Project?',
-        validate: (answer) => { if (answer.length < 1) {
-            return console.log("A valid GitHub username is required.");
-        }
-        return true; }
-    },
+const questions = [
+  {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the title of your Project?',
+      validate: (answer) => { if (answer.length < 1) {
+          return console.log("A valid GitHub username is required.");
+      }
+      return true; }
+  },
 
-    {
-        type: 'input',
-        message: "What is your GitHub username? ",
-        name: 'githubName',
-        validate: (answer) => { if (answer.length < 1) {
-            return console.log("A valid GitHub username is required.");}
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "What is the name of your GitHub repo?",
-        name: 'githubRepo',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid GitHub repo is required for a badge.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'projectLicense'
-    },
-    {
-        type: 'input',
-        message: "Write a description of your project.",
-        name: 'projectDescription',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid project description is required.");
-            }
-            return true;
-        }
-    },
-    {
+  {
       type: 'input',
-      name: 'projectInstallation',
-      message: 'What are the guidelines for installing your project application?',
-    },
-    {
+      message: "What is your GitHub username? ",
+      name: 'githubName',
+      validate: (answer) => { if (answer.length < 1) {
+          return console.log("A valid GitHub username is required.");}
+          return true;
+      }
+  },
+  {
       type: 'input',
-      name: 'projectContribution',
-      message: 'What are the guidelines for contribution?',
-    },
-    {
+      message: "What is the name of your GitHub repo?",
+      name: 'githubRepo',
+      validate: function (answer) {
+          if (answer.length < 1) {
+              return console.log("A valid GitHub repo is required for a badge.");
+          }
+          return true;
+      }
+  },
+  {
+      type: 'list',
+      message: "Choose a license for your project.",
+      choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+      name: 'projectLicense'
+  },
+  {
       type: 'input',
-      name: 'projectUsage',
-      message: 'Who is allowed to use your project?',
-    },
-    {
-        type: 'input',
-        message: "Provide any tests written for your application and provide examples on how to run them.",
-        name: 'projectTests'
-    },
-  ])
+      message: "Write a description of your project.",
+      name: 'projectDescription',
+      validate: function (answer) {
+          if (answer.length < 1) {
+              return console.log("A valid project description is required.");
+          }
+          return true;
+      }
+  },
+  {
+    type: 'input',
+    name: 'projectInstallation',
+    message: 'What are the guidelines for installing your project application?',
+  },
+  {
+    type: 'input',
+    name: 'projectContribution',
+    message: 'What are the guidelines for contribution?',
+  },
+  {
+    type: 'input',
+    name: 'projectUsage',
+    message: 'Who is allowed to use your project?',
+  },
+  {
+      type: 'input',
+      message: "Provide any tests written for your application and provide examples on how to run them.",
+      name: 'projectTests'
+  },
+];
 
+
+// function to initialize program
+function init() {
+
+  inquirer.prompt(questions) 
 
 // TODO: Create a function to write README file
 .then((answers) => {
@@ -88,18 +91,15 @@ inquirer
     fs.writeFile(`${answers.projectTitle}.md`, readmePageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created README File!')
     );
-  });
-
-
-
+ } );}
 
 
 const generateREADME = ({ projectTitle, githubName, githubRepo, projectDescription, projectInstallation, projectContribution, projectLicense, projectTests, projectQuestions, projectUsage}) =>
   
  ` 
- # ${projectTitle}
- ## ${githubName}
-### ${githubRepo}
+ # Project Title: ${projectTitle}
+ ## Github Username: ${githubName}
+### Github Page ${githubRepo}
 
 
 
@@ -133,11 +133,11 @@ const generateREADME = ({ projectTitle, githubName, githubRepo, projectDescripti
 
  ###  Questions
  ${projectQuestions}
+`
 
 
+ // function call to initialize program
+ init();
 
-
-
-
- `
+ 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return 
+`
+  # ${projectTitle}
+ ## ${githubName}
+### ${githubRepo}
 
-`;
+
+
+ # Table of Contents 
+ 1. Description
+ 2. Installation
+ 3. Usage Guidelines
+ 4. Contributing
+ 5. Tests
+ 6. Questions
+
+ ## Description
+ ${projectDescription}
+
+
+ ## Installation
+ ${projectInstallation}
+
+ ### Usage
+ ${projectUsage}
+
+ ### License
+ ${projectLicense}
+
+
+ ## Contributing
+ ${projectContribution}
+
+ #### Tests
+ ${projectTests}
+
+ ###  Questions
+ ${projectQuestions}
+
+ `
+;
 }
+
 
 module.exports = generateMarkdown;
