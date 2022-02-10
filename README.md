9ebba93175d094d6467f83deee6eed3948af984b
ahmed <mueed@Usc.edu>
2022-02-10 17:17:43 -0500

    Got the basic questionaire to work and successfully created the ReadMe

---
+++ README.md
@@ -0,0 +1,9 @@
+ ReadMe Generator
+
+ It is a CLI that creates a repo based on user inputs
+ 
+ you just need to donwload it and make sure you have inquirerer
+
+ ill take something : )
+
+ anyone
--- index.js
+++ index.js
@@ -1,13 +1,92 @@
 // TODO: Include packages needed for this application
 
+// We are requiring fs which is short for 'file system' in order to use the append or write file methods to create a new md file for the ReadMe
+const fs = require('fs');
+
+// We are requiring 'inquirer' which is what we installed using npm install in order to more easily create this cli interface
+const inquirer = require('inquirer');
+
+
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'projectTitle',
+      message: 'What is the title of your Project?',
+    },
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README 
+
+    {
+      type: 'input',
+      name: 'projectDescription',
+      message: 'Describe your project',
+    },
+
+    {
+      type: 'input',
+      name: 'projectInstallation',
+      message: 'What are the guidelines for installing your project application?',
+    },
+    {
+      type: 'input',
+      name: 'projectContribution',
+      message: 'What are the guidelines for contribution?',
+    },
+    {
+      type: 'input',
+      name: 'projectUsage',
+      message: 'Who is allowed to use your project?',
+    },
+  ])
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+.then((answers) => {
+    const readmePageContent = generateREADME(answers);
+
+    fs.writeFile('README.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README File!')
+    );
+  });
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+
+
+
+const generateREADME = ({ projectTitle, projectDescription, projectInstallation, projectContribution, projectUsage}) =>
+  
+ ` ${projectTitle}
+
+ ${projectDescription}
+ 
+ ${projectInstallation}
+
+ ${projectContribution}
+
+ ${projectUsage}`; ;
+
+
+
+
+
+
+
+  // Name is how you store it
+  // Validate with Validate the answers
+
+
+  //Write a function to give you the html output
+
+
+
