02df84d06b92c790a7a27b84f426b40c1dde0062
ahmed <mueed@Usc.edu>
2022-02-10 17:59:29 -0500

    Added the rest of the questions and formatted the md more

---
+++ Readme Generator.md
@@ -0,0 +1,44 @@
+ 
+ # Readme Generator
+ ## readme
+### blah
+
+
+
+ # Table of Contents 
+ 1. Description
+ 2. Installation
+ 3. Usage Guidelines
+ 4. Contributing
+ 5. Tests
+ 6. Questions
+
+ ## Description
+ dkdkd
+
+
+ ## Installation
+ dkjfdklf
+
+ ### Usage
+ 
+
+ ### License
+ Apache License 2.0
+
+
+ ## Contributing
+ 
+
+ #### Tests
+ 
+
+ ###  Questions
+ undefined
+
+
+
+
+
+
+ 
--- index.js
+++ index.js
@@ -8,24 +8,56 @@ const inquirer = require('inquirer');
 
 
 
-
 // TODO: Create an array of questions for user input
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'projectTitle',
-      message: 'What is the title of your Project?',
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your Project?',
+        validate: (answer) => { if (answer.length < 1) {
+            return console.log("A valid GitHub username is required.");
+        }
+        return true; }
     },
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README 
 
     {
-      type: 'input',
-      name: 'projectDescription',
-      message: 'Describe your project',
+        type: 'input',
+        message: "What is your GitHub username? ",
+        name: 'githubName',
+        validate: (answer) => { if (answer.length < 1) {
+            return console.log("A valid GitHub username is required.");}
+            return true;
+        }
     },
-
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'githubRepo',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'projectLicense'
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'projectDescription',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
     {
       type: 'input',
       name: 'projectInstallation',
@@ -41,8 +73,12 @@ inquirer
       name: 'projectUsage',
       message: 'Who is allowed to use your project?',
     },
+    {
+        type: 'input',
+        message: "Provide any tests written for your application and provide examples on how to run them.",
+        name: 'projectTests'
+    },
   ])
-
 
 
 // TODO: Create a function to write README file
@@ -57,46 +93,51 @@ inquirer
 
 
 
-const generateREADME = ({ projectTitle, projectDescription, projectInstallation, projectContribution, projectUsage}) =>
+
+const generateREADME = ({ projectTitle, githubName, githubRepo, projectDescription, projectInstallation, projectContribution, projectLicense, projectTests, projectQuestions, projectUsage}) =>
   
  ` 
- ${projectTitle}
+ # ${projectTitle}
+ ## ${githubName}
+### ${githubRepo}
+
+
 
  # Table of Contents 
+ 1. Description
+ 2. Installation
+ 3. Usage Guidelines
+ 4. Contributing
+ 5. Tests
+ 6. Questions
 
  ## Description
  ${projectDescription}
+
 
  ## Installation
  ${projectInstallation}
 
  ### Usage
- ${projectUsage};
+ ${projectUsage}
+
  ### License
+ ${projectLicense}
+
 
  ## Contributing
  ${projectContribution}
 
-
  #### Tests
+ ${projectTests}
 
- ###  Questions`
- 
- 
-
- ;
+ ###  Questions
+ ${projectQuestions}
 
 
 
 
 
 
-
-  // Name is how you store it
-  // Validate with Validate the answers
-
-
-  //Write a function to give you the html output
-
-
+ `
 
